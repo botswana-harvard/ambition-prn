b6588e2d581785577e2c040885c6a7b967e1235a
erikvw <ew2789@gmail.com>
2017-12-18 16:59:11 +0200

    import mommy

---
+++ ambition_prn/mommy_recipes.py
@@ -0,0 +1,28 @@
+from edc_constants.constants import YES
+from faker import Faker
+from model_mommy.recipe import Recipe
+
+from .models import DeathReport, StudyTerminationConclusion
+from .models import ProtocolDeviationViolation
+
+fake = Faker()
+
+deathreport = Recipe(
+    DeathReport,
+    study_day=1,
+    death_as_inpatient=YES,
+    cause_of_death_study_doctor_opinion='art_toxicity',
+    cause_other_study_doctor_opinion='None',
+    cause_tb_study_doctor_opinion=None,
+    cause_of_death_tmg1_opinion='art_toxicity',
+    cause_other_tmg1_opinion='None',
+    cause_tb_tmg1_opinion=None,
+    cause_of_death_tmg2_opinion='art_toxicity',
+    cause_other_tmg2_opinion='None',
+    cause_tb_tmg2_opinion=None,
+    narrative_summary=(
+        'adverse event resulted in death due to cryptococcal meningitis'))
+
+studyterminationconclusion = Recipe(StudyTerminationConclusion)
+
+protocoldeviationviolation = Recipe(ProtocolDeviationViolation)
