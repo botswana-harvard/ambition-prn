24172e345b27001d46dca1b27b85598e38571ba6
erikvw <ew2789@gmail.com>
2018-01-16 19:15:05 +0200

    add Login required mixin

--- ambition_prn/views/home_view.py
+++ ambition_prn/views/home_view.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 
 from edc_base.view_mixins import EdcBaseViewMixin
@@ -8,7 +6,3 @@ from edc_base.view_mixins import EdcBaseViewMixin
 class HomeView(EdcBaseViewMixin, TemplateView):
 
     template_name = 'ambition_prn/home.html'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
